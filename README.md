0cb6df6321924e2e92dc7484d6da411ebd8e34cd
gitgrimbo <gitgrimbo@gmail.com>
2016-09-16 21:02:49 +0100

    Spin up HTTP server to serve local HAR files if required

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ node index.js
 Options:
   --hv-url      HAR Viewer URL.
             [required] [default: "http://gitgrimbo.github.io/harviewer/master/"]
-  --har         HAR URL. Can be provided zero or more times.
-  --harp        HARP URL. Can be provided zero or more times.
+  --har         HAR URL or file path. Can be provided zero or more times.
+  --harp        HARP URL or file path. Can be provided zero or more times.
   --hv-pageIdx  Index of the page to open.                          [default: 0]
   --hv-width    Width of viewport.                               [default: 1280]
   --hv-height   Height of viewport. Defaults to full document.body.clientHeight.
@@ -21,7 +21,7 @@ Options:
   --help        Show help                                              [boolean]
 ````
 
-E.g.
+### Serve HAR files from standalone server
 
 `node index.js --har http://localhost:8080/hars/myhar.har --hv-pageIdx 4 --hv-width 1024`
 
@@ -32,6 +32,22 @@ used to serve local files with CORS.  For example:
 npm install -g http-server
 cd FOLDER_CONTAINING_HARS
 http-server --cors
+````
+
+### Serve HAR files using in-built server
+
+If the `--har` argument is a valid file path, a local HTTP server is started
+to serve this file to HAR Viewer. E.g.:
+
+`node index.js --har local-file.har`
+
+will start a HTTP server (on a random available port) and output something
+like the following:
+
+````
+Started local server on port 3933
+Creating page
+Opening http://gitgrimbo.github.io/harviewer/master/?har=http://localhost:3933/c:/path/to/local-file.har
 ````
 
 ## As library
@@ -58,7 +74,6 @@ screenshot.screenshot(opts);
 
 # TODO
 
-- In-built HTTP server to serve local HAR files.
 - Customisation
   - Timeouts.
   - Remove tabs/button/extraneous chrome.
--- index.js
+++ index.js
@@ -1,5 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const fileUrl = require("file-url");
 const buildArgv = require("./lib/build-argv");
 const screenshot = require("./lib/screenshot");
+const { createServer } = require("./lib/server");
+
+function localAndRemote(paths) {
+  const ctx = {
+    local: [],
+    remote: []
+  };
+  return paths.reduce((ctx, p) => {
+    const { protocol } = url.parse(p);
+
+    if (protocol) {
+      ctx.remote.push(p);
+    } else {
+      const resolvedPath = path.resolve(p);
+      const stats = fs.statSync(resolvedPath);
+      if (stats.isFile()) {
+        ctx.local.push(resolvedPath);
+      } else {
+        throw new Error(p + " was neither a file nor a URL");
+      }
+    }
+
+    return ctx;
+  }, ctx);
+}
+
+function mapFilePathToLocalUrl(port) {
+  return function(path) {
+    const u = fileUrl(path);
+    const parsed = url.parse(u);
+    return "http://localhost:" + port + parsed.pathname;
+  };
+}
 
 function main(cli) {
   const argv = cli.argv;
@@ -18,18 +55,58 @@ function main(cli) {
     return values.map(v => name + "=" + v).join("&");
   }
 
-  const har = ensureArray(argv.har);
-  const harp = ensureArray(argv.harp);
-  const url = argv["hv-url"] + "?" + params("har", har) + params("harp", harp);
+  function doScreenshot(har, harp) {
+    const url = argv["hv-url"] + "?" +
+      params("har", har.local) +
+      params("harp", harp.local) +
+      params("har", har.remote) +
+      params("harp", harp.remote);
 
-  const opts = {
-    hvUrl: url,
-    hvPageIdx: argv["hv-pageIdx"],
-    hvWidth: argv["hv-width"],
-    hvHeight: argv["hv-height"],
-    dest: argv.dest
-  };
-  screenshot.screenshot(opts);
+    const opts = {
+      hvUrl: url,
+      hvPageIdx: argv["hv-pageIdx"],
+      hvWidth: argv["hv-width"],
+      hvHeight: argv["hv-height"],
+      dest: argv.dest
+    };
+
+    return screenshot.screenshot(opts).then(() => {
+      console.log("Screenshot done.");
+    }, err => {
+      console.error("There was an error trying to capture screenshot...");
+      console.error(err);
+    });
+  }
+
+  function useLocalServer(har, harp) {
+    const server = createServer();
+    const stopServer = err => {
+      if (err) {
+        console.error(err);
+      }
+      console.log("Stopping local server.");
+      server.stop();
+    };
+    return server.start().then(function(server) {
+      const port = server.address().port;
+
+      console.log(`Started local server on port ${port}`);
+
+      har.local = har.local.map(mapFilePathToLocalUrl(port));
+      harp.local = harp.local.map(mapFilePathToLocalUrl(port));
+
+      return doScreenshot(har, harp);
+    }).then(stopServer, stopServer);
+  }
+
+  const har = localAndRemote(ensureArray(argv.har));
+  const harp = localAndRemote(ensureArray(argv.harp));
+
+  if (har.local.length > 0 || harp.local.length > 0) {
+    useLocalServer(har, harp);
+  } else {
+    doScreenshot(har, harp);
+  }
 }
 
 const cli = buildArgv();
